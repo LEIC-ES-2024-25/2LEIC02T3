207268ca13d87ba874aa95468539b8cbec7340c2
Joao Quental <up202307755@up.pt>
2025-03-22 19:28:38 +0000

    updated challenges class for shower timer challenge

--- lib/models/challenge.dart
+++ lib/models/challenge.dart
@@ -8,7 +8,9 @@ class Challenge {
   int currentSteps; // Current steps completed (mutable)
   String carFreeStatus; // For Car-Free Day challenge
   String bikeRideStatus; // New property for bike-riding status
-
+  bool isTimerRunning; // For tracking if the timer is running
+  int elapsedTime; // Elapsed time in seconds
+  String showerStatus; // Status of the shower (porcalhÃ£o, quick-shower, long-shower)
 
   Challenge({
     required this.title,
@@ -18,5 +20,8 @@ class Challenge {
     this.currentSteps = 0, // Start with 0 steps completed
     this.carFreeStatus = "car-free by now", // Default status for Car-Free Day
     this.bikeRideStatus = "not done yet", // Default status for bike-riding
+    this.isTimerRunning = false, // Timer is not running initially
+    this.elapsedTime = 0, // Elapsed time starts at 0
+    this.showerStatus = "not started", // Initial shower status
   });
 }
