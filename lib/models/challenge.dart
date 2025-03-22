378349d7003856b1e3331786b613321cd3846f64
Joao Quental <up202307755@up.pt>
2025-03-22 19:24:50 +0000

    update challenges class for car and bike challenges

--- lib/models/challenge.dart
+++ lib/models/challenge.dart
@@ -1,9 +1,13 @@
+// lib/models/challenge.dart
+
 class Challenge {
   final String title;
   final String description;
   final int points;
   final int totalSteps; // Total steps required for this challenge
   int currentSteps; // Current steps completed (mutable)
+  String carFreeStatus; // For Car-Free Day challenge
+  String bikeRideStatus; // New property for bike-riding status
 
 
   Challenge({
@@ -12,5 +16,7 @@ class Challenge {
     required this.points,
     this.totalSteps = 0, // Default to 0 if not a step-based challenge
     this.currentSteps = 0, // Start with 0 steps completed
+    this.carFreeStatus = "car-free by now", // Default status for Car-Free Day
+    this.bikeRideStatus = "not done yet", // Default status for bike-riding
   });
 }
