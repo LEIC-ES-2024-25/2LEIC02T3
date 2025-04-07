b99b44c06ded91d67ea58b6f9c7277aa072a87bf
Luís Ferreira <up202007664@up.pt>
2025-04-07 03:22:32 +0100

    permission implemented

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,7 @@ import 'providers/badges_provider.dart';
 import 'providers/challenges_provider.dart';
 
 void main() {
-  // Ensure Flutter is initialized
-  WidgetsFlutterBinding.ensureInitialized();
+  
   runApp(
     MultiProvider(
       providers: [
